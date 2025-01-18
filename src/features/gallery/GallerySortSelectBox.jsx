c24a78c0b263eec3060ba5beb4c490c7c77eecf3
Mahan <reza.mahan10@gmail.com>
2025-01-18 20:31:45 +0330

    change sort language change...

--- src/features/gallery/GallerySortSelectBox.jsx
+++ src/features/gallery/GallerySortSelectBox.jsx
@@ -1,12 +1,19 @@
+import { useLanguage } from "../../context/useLanguageContext";
+
 function GallerySortSelectBox() {
+  const { language } = useLanguage();
   return (
     <select
       name="preferSearch"
-      className="w-full md:w-fit border-none focus:ring-transparent text-sm p-1 font-semibold rounded-md px-2 shadow-sm shadow-slate-800 md:mb-0 mb-2"
+      className="w-full md:w-fit border-none focus:ring-transparent text-sm p-1 font-semibold rounded-md shadow-sm shadow-slate-800 md:mb-0 mb-2"
       onChange={(e) => console.log(e.target.value)}
     >
-      <option value="New">Based on Newest</option>
-      <option value="Old">Based on Oldest</option>
+      <option value="new">
+        {language === "en" ? "Based on Newest" : "براساس جدید ترین"}
+      </option>
+      <option value="old">
+        {language === "en" ? "Based on Oldest" : "براساس قدیمی ترین"}
+      </option>
     </select>
   );
 }
