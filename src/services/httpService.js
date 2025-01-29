aa2b1f1e6f8ba304dd6dc613832b7f2698d6c907
Mahan <reza.mahan10@gmail.com>
2025-01-29 19:15:24 +0330

    ...

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -7,6 +7,31 @@ const app = axios.create({
   withCredentials: true,
 });
 
+app.interceptors.request.use(
+  (res) => res,
+  (error) => Promise.reject(error)
+);
+app.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalConfig = error.config;
+
+    if (error.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(`${BASE_URL}/users/login`, {
+          withCredentials: true,
+        });
+
+        if (data) return app(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 const http = {
   get: app.get,
   post: app.post,
