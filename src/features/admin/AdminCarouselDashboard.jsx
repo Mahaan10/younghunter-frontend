572425d8cc6be75250226b277ae0b031c716d00a
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-02-13 10:34:16 +0330

    image access completed, include create image, carousel feature, edit image and delete image...

--- src/features/admin/AdminCarouselDashboard.jsx
+++ src/features/admin/AdminCarouselDashboard.jsx
@@ -3,14 +3,13 @@ import { useLanguage } from "../../context/useLanguageContext";
 import AccessModal from "../../ui/AccessModal";
 import ImagesHeader from "./images/ImagesHeader";
 import ImagesTable from "./images/ImagesTable";
-import CarouselHeader from "./carousel/CarouselHeader";
-import CarouselTable from "./carousel/CarouselTable";
 
 function AdminCarouselDashboard() {
   const { language } = useLanguage();
   const [isImageAccessOpen, setIsImageAccessOpen] = useState(false);
   const [isCarouselImageAccessOpen, setIsCarouselImageAccessOpen] =
     useState(false);
+  const [sortImages, setSortImages] = useState("all");
 
   return (
     <>
@@ -21,31 +20,15 @@ function AdminCarouselDashboard() {
         >
           {language === "en" ? "Image Access" : "دسترسی به عکس ها"}
         </button>
-        <button
-          className="bg-green-600 adminRoleBtn text-black"
-          onClick={() => setIsCarouselImageAccessOpen((prev) => !prev)}
-        >
-          {language === "en" ? "Carousel Access" : "دسترسی به کاروسل"}
-        </button>
       </div>
-
-      {isCarouselImageAccessOpen && (
-        <AccessModal
-          title={language === "en" ? "Carousel Access" : "دسترسی به کاروسل"}
-          onClose={() => setIsCarouselImageAccessOpen(false)}
-        >
-          <CarouselHeader />
-          <CarouselTable />
-        </AccessModal>
-      )}
 
       {isImageAccessOpen && (
         <AccessModal
           title={language === "en" ? "Image Access" : "دسترسی به عکس"}
           onClose={() => setIsImageAccessOpen(false)}
         >
-          <ImagesHeader />
-          <ImagesTable />
+          <ImagesHeader sortImages={sortImages} setSortImages={setSortImages} />
+          <ImagesTable sortImages={sortImages} />
         </AccessModal>
       )}
     </>
--- src/features/admin/images/ImagesTable.jsx
+++ src/features/admin/images/ImagesTable.jsx
@@ -1,19 +1,28 @@
 import { useLanguage } from "../../../context/useLanguageContext";
 import useImages from "../../../hooks/useImages";
 import Table from "../../../ui/Table";
-import ImagesRow from "./imagesRow";
+import ImagesRow from "./ImagesRow";
 
-function ImagesTable() {
+function ImagesTable({ sortImages }) {
   const { images } = useImages();
   const { language } = useLanguage();
 
+  let sortedImages = images;
+
+  sortImages === "carouselImages"
+    ? (sortedImages = images.filter(
+        (image) => image.isFeaturedCarousel === true
+      ))
+    : sortImages === "not-carouselImages"
+    ? (sortedImages = images.filter(
+        (image) => image.isFeaturedCarousel === false
+      ))
+    : images;
 
   if (!images.length)
     return (
       <p className="text-neutral-200">
-        {language === "en"
-          ? "There is no Image!"
-          : "عکسی یافت نشد!"}
+        {language === "en" ? "There is no Image!" : "عکسی یافت نشد!"}
       </p>
     );
 
@@ -24,11 +33,13 @@ function ImagesTable() {
         <th>{language === "en" ? "Images" : "عکس ها"}</th>
         <th>{language === "en" ? "English Title" : "عنوان به انگلیسی"}</th>
         <th>{language === "en" ? "Persian Title" : "عنوان به فارسی"}</th>
-        <th>{language === "en" ? "Include in Carousel?" : "در کاروسل وجود دارد؟"}</th>
+        <th>
+          {language === "en" ? "Include in Carousel?" : "در کاروسل وجود دارد؟"}
+        </th>
         <th>{language === "en" ? "Operations" : "عملیات"}</th>
       </Table.Header>
       <Table.Body>
-        {images.map((image, index) => (
+        {sortedImages.map((image, index) => (
           <ImagesRow key={image._id} image={image} index={index} />
         ))}
       </Table.Body>
