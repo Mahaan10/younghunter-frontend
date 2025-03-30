92eeccbe28f122083f49441bb1a8de5a5567185a
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-03-31 02:41:30 +0330

    modify admin albums page test...

--- src/features/admin/album/AdminAlbumsPageLayout.jsx
+++ src/features/admin/album/AdminAlbumsPageLayout.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AlbumsHeader from "./AlbumsHeader";
+import AlbumsTable from "./AlbumsTable";
 
 function AdminAlbumsPageLayout() {
-  return <div>AdminAlbumsPageLayout</div>;
+  return (
+    <>
+      <AlbumsHeader />
+      <AlbumsTable />
+    </>
+  );
 }
 
 export default AdminAlbumsPageLayout;
