dc1e65d06064e66319a284a235e90dbb05eabc9f
Mahan <reza.mahan10@gmail.com>
2025-04-04 17:00:00 +0330

    ......

--- src/features/admin/subAlbum/image/AdminSubAlbumImagesPageLayout.jsx
+++ src/features/admin/subAlbum/image/AdminSubAlbumImagesPageLayout.jsx
@@ -2,14 +2,20 @@ import { useParams } from "react-router-dom";
 import ImageHeader from "./ImageHeader";
 import ImagesTable from "./ImageTable";
 import useAlbums from "../../../../hooks/useAlbums";
+import Loading from "../../../../ui/Loading";
+import toast from "react-hot-toast";
 
 function AdminSubAlbumImagesPageLayout() {
-  const { albums } = useAlbums();
+  const { albums, error, isError, isLoading } = useAlbums();
   const { albumId, subAlbumId } = useParams();
   const album = albums?.find((album) => album._id === albumId);
   const subAlbum = album?.subAlbums?.find(
     (subAlbum) => subAlbum._id === subAlbumId
   );
+
+  if (isLoading) return <Loading />;
+  if (isError)
+    return toast.error(error?.response?.data?.message || error?.message);
 
   return (
     <>
